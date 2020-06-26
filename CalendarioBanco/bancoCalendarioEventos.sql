0a873b63f4f95f56c25431bc504ef21ef869d20f
Abejyou <abe.aosora@gmail.com>
2020-06-25 20:09:48 -0400

    tabelas criadas
    evento e notificação

--- CalendarioBanco/bancoCalendarioEventos.sql
+++ CalendarioBanco/bancoCalendarioEventos.sql
@@ -1,19 +1,38 @@
+ SET NAMES utf8 ;
 
-CREATE TABLE usuario (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(50) NOT NULL,
-email VARCHAR(50) NOT NULL,
-senha VARCHAR(50) NOT NULL,
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `usuario` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `senha` varchar(50) NOT NULL,
+  `reg_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE Table evento(
-	id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    titulo_evento VARCHAR(50) NOT NULL,
-    local_evento VARCHAR(50) NOT NULL,
-    data_inicio datetime NOT NULL,
-    data_fim datetime NOT NULL,
-    descricao varchar(500),
-    
-)
+DROP TABLE IF EXISTS `evento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `evento` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `titulo_evento` varchar(50) NOT NULL,
+  `local_evento` varchar(50) NOT NULL,
+  `data_inicio` datetime NOT NULL,
+  `data_fim` datetime NOT NULL,
+  `descricao` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+DROP TABLE IF EXISTS `notificacao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `notificacao` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_evento` int(10) unsigned NOT NULL,
+  `data_notificacao` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_notificacao_evento_idx` (`id_evento`),
+  CONSTRAINT `fk_notificacao_evento` FOREIGN KEY (`id_evento`) REFERENCES `evento` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
