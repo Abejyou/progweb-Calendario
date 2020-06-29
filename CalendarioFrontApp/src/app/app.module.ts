c1cf552272a2847c93891012138c6bbe1f937dd9
Abejyou <abe.aosora@gmail.com>
2020-06-28 23:43:48 -0400

    ajustes form eventos

--- CalendarioFrontApp/src/app/app.module.ts
+++ CalendarioFrontApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 //Modules
 import { AppRoutingModule } from './app-routing.module';
@@ -43,7 +44,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],  
   providers: [EventosService],
   bootstrap: [AppComponent]
